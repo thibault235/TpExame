e30b405ee1fc856c81556221613506dc58e6db62
thibault235 <116006053+thibault235@users.noreply.github.com>
2024-10-24 15:38:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const	http	=	require('http');	
-const	server	=	http.createServer((req,	res)	=>	{	
-res.statusCode	=	200;	
-res.setHeader('Content-Type',	'text/plain');	
-res.end('Hello	World\n');	
-});	
-const	port	=	process.env.PORT	||	3000;	
-server.listen(port,	()	=>	{	
-console.log(`Server	running	at	http://localhost:${port}/`);	
+const http = require('http');
+ 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Azure ( Estiam ) \n');
+});
+ 
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
